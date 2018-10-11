4609f8e81966ee9126328d26e065eb2c6bc8c72a
KousukeSaitou <k_saitou@chatlune.jp>
2018-10-11 17:43:44 +0900

    解説を追加

--- app/src/main/java/com/sample/android_resources/MainActivity.java
+++ app/src/main/java/com/sample/android_resources/MainActivity.java
@@ -2,6 +2,8 @@ package com.sample.android_resources;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // getTextで文字列を取得
+        String str1 = getString(R.string.sample1);
+        String str2 = getString(R.string.sample2,"りんご",3);
+
+        // ログ出力
+        Log.d("sample","str1:"+str1);
+        Log.d("sample","str2:"+str2);
+
+        // ビューにも反映
+        TextView text1 = (TextView)findViewById(R.id.textView1);
+        text1.setText(str1);
+        TextView text2 = (TextView)findViewById(R.id.textView2);
+        text2.setText(str2);
+
+
+        // コードからマークアップを反映させる
+//        TextView text3 = (TextView)findViewById(R.id.textView1);
+//        text3.setText(getText(R.string.sample4));
+
     }
 }
